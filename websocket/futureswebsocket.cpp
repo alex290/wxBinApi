f61044d43941d8f3eb8ac07ed74e664c50adb548
Alexandr <alex@webgoal.ru>
2023-07-07 12:53:23 +0300

    Update

--- websocket/futureswebsocket.cpp
+++ websocket/futureswebsocket.cpp
@@ -9,11 +9,11 @@ FuturesWebSocket::~FuturesWebSocket()
 }
 
 // https://binance-docs.github.io/apidocs/futures/en/#kline-candlestick-streams
-void FuturesWebSocket::klines(std::vector<std::string> symbols)  // Запуск свечных данных Websocket
+void FuturesWebSocket::klines(std::vector<std::string> symbols, std::string inerval)  // Запуск свечных данных Websocket
 {
 }
 
 // https://binance-docs.github.io/apidocs/futures/en/#diff-book-depth-streams
-void FuturesWebSocket::diffDepth(std::vector<std::string> symbols)  // Запуск данных стакана (книга заявок)
+void FuturesWebSocket::diffDepth(std::vector<std::string> symbols,  int speed)  // Запуск данных стакана (книга заявок)
 {
 }
--- websocket/futureswebsocket.h
+++ websocket/futureswebsocket.h
@@ -10,10 +10,12 @@ public:
     ~FuturesWebSocket();
 
     // https://binance-docs.github.io/apidocs/futures/en/#kline-candlestick-streams
-    void klines(std::vector<std::string> symbols);     // Запуск свечных данных Websocket
+    // interval 1m, 3m, 5m, 15m, 30m, 1h, 2h, 4h, 6h, 8h, 12h, 1d, 3d, 1w, 1M
+    void klines(std::vector<std::string> symbols, std::string inerval);     // Запуск свечных данных Websocket
 
     // https://binance-docs.github.io/apidocs/futures/en/#diff-book-depth-streams
-    void diffDepth(std::vector<std::string> symbols);  // Запуск данных стакана (книга заявок)
+    // Update Speed: 250ms, 500ms, 100ms
+    void diffDepth(std::vector<std::string> symbols, int speed = 0);  // Запуск данных стакана (книга заявок)
 
 private:
     DatGlobBinApi* dataGlobal;
