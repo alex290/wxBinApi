7ff007ebdbfd0462cf70802c3703a5150975817b
Alexandr <alex@webgoal.ru>
2023-07-08 13:05:13 +0300

    Update

--- websocket/futureswebsocket.cpp
+++ websocket/futureswebsocket.cpp
@@ -11,6 +11,7 @@ FuturesWebSocket::~FuturesWebSocket()
 // https://binance-docs.github.io/apidocs/futures/en/#kline-candlestick-streams
 void FuturesWebSocket::klines(std::vector<std::string> symbols, std::string inerval)  // Запуск свечных данных Websocket
 {
+    std::cout << "Start klines - symbols: " << symbols[0] << "; interval: " << inerval << std::endl;
 }
 
 // https://binance-docs.github.io/apidocs/futures/en/#diff-book-depth-streams
